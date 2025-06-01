4b2de8b7fd50b260e787b275f2075eb8e795a896
cwesi-djin <cwesidjin1@gmail.com>
2025-06-01 14:58:22 +0000

    testing

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -14,5 +14,10 @@ match operation:
         result = num1 * num2
         print(f"The result is {result}.")
     case "/":
-        result = num1 / num2
-        print(f"The result is {result}.")
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print(f"The result is {result}.")
+    case _:
+        print("Invalid operation.")

a8a4fcdad450d23a77b03fa438c93071ee023844
cwesi-djin <cwesidjin1@gmail.com>
2025-06-02 16:59:58 +0000

    fns and dsa

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -18,3 +18,10 @@ def display_item():
         for i in [shopping_list]:
             print(i)
             i += 1
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")

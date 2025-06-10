cf5eb9e4469e0bbf4d7b19c4b4776b88b99a1714
cwesi-djin <cwesidjin1@gmail.com>
2025-06-10 18:12:27 +0000

    Object oriented programming test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,12 +7,12 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_test_addition(self):
         self.assertEqual(self.calc.add(3, 4), 7)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 4), 6)
         self.assertEqual(self.calc.subtract(0, 0), 0)
         self.assertEqual(self.calc.subtract(-5, -5), 0)

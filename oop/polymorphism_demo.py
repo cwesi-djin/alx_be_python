8203f8eebb0212704d583b94110c8fdb78840aca
cwesi-djin <cwesidjin1@gmail.com>
2025-06-17 16:49:02 +0000

    oop-task 2

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -9,7 +9,7 @@ class Rectangle(Shape):
         self.length = length 
         self.width = width
 
-    def area(self, le):
+    def area(self):
         return self.length * self.width
         
 
